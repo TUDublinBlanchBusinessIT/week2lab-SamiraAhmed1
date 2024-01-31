ec66477acad3824ffc3ba105ea5ea47de1ecf884
B00156786 Samira Ahmed <B00156786@mytudublin.ie>
2024-01-31 12:07:10 +0000

    PartB.py completed

--- PartB.py
+++ PartB.py
@@ -2,16 +2,14 @@
 #31/01/2024
 #A Program to Print Steps 1 to 9 to the Screen
 
-def printSteps():
-    print("Step 4")
-    print("Step 5")
-    print("Step 6")
-
+def printSteps(a, b):
+    for i in range(a, b+1):
+        print(f"Steps {i}")
     
 print("Step 1")
 print("Step 2")
 print("Step 3")
-printSteps()
+printSteps(4, 6)
 print("Step 7")
 print("Step 8")
 print("Step 9")
